c2b0cb84005cf4d6688c9f484b2040a15d77aceb
Rodion23 <rodion23@mail.ru>
2023-02-09 21:23:36 +0700

    Add HW1

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 1: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2 */
+
+int InputInt(string message)
+{
+    System.Console.Write($"{message} > ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int[] GenerateArray(int len)
+{
+    int[] array = new int[len];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(100, 999);
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    foreach (int item in array)
+    {
+        System.Console.Write($"{item}\t");
+    }
+    System.Console.WriteLine();
+}
+int EvenNumber(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int len = InputInt("Введите длину массива");
+int[] array = GenerateArray(len);
+PrintArray(array);
+System.Console.WriteLine($"Количество четных чисел в мвссиве: {EvenNumber(array)}");
--- HW2/Program.cs
+++ HW2/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 2: Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0 */
+int[] GenerateArray(int len = 8)
+{
+    int[] array = new int[len];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(0, 100);
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    foreach (int item in array)
+    {
+        System.Console.Write($"{item}\t");
+    }
+    System.Console.WriteLine();
+}
+// int[] Inversion(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i=i+2)
+//     {
+//         int sum =0;
+//         sum += array[i];
+//     }
+//     return sum;
+// }
+int length = 10;
+int[] array = new int[length];
+Random rnd = new Random();
+int sum = 0;
+for (int i = 0; i < length; i++) {
+    array[i] = rnd.Next(1, 70);
+    if (array[i] % 2 != 0) {
+        sum += array[i];
+    }
+}
+int ar =Convert.ToInt32(array);
+System.Console.WriteLine(ar);
+Console.WriteLine(sum);
